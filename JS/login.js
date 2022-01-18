40a2c985670b6a8cd5744d35a0cfd933f1295361
marianosin <marianoradusky.19@gmail.com>
2022-01-18 14:59:45 -0300

    El login no anda como espero

--- JS/login.js
+++ JS/login.js
@@ -1,12 +1,12 @@
-//Usuario modelo
-const username = 'mariano'
-const password = 'radusky'
+let DASHBOARD_USERS = ''
+
+import { App } from "./components/App.js";
+import { User } from "./components/User.js";
 
 
-//Login funcion para ver si está registrado
 function login(username, password) {
-    asked_username = prompt("Username")
-    asked_password = prompt("Password")
+    let asked_username = prompt("Username")
+    let asked_password = prompt("Password")
 
     if ((username==asked_username) && (password==asked_password)){
         return true
@@ -15,12 +15,44 @@ function login(username, password) {
     }
 }
 
-login = login(username,password)
+let tempList = JSON.parse(localStorage.getItem(DASHBOARD_USERS))
+console.log(tempList)
 
+if (tempList == null || tempList == undefined){
+    // Inicializo la app
+
+    const dashboard = new App()
+
+    // Creo un usuario modelo 
+    const user1 = new User('Mariano Radusky','mradusky', 'Radusky', '1993-07-19')
+
+    dashboard.addUser(user1)
+
+    console.log('Estoy en el if')
+
+    localStorage.setItem(DASHBOARD_USERS,JSON.stringify(dashboard));
+
+
+}else{
+    console.log('Estoy en el else')
+    let dashboard = tempList
+
+
+
+
+
+    
+}
+
+
+let login_status = login(dashboard.users[0].username,dashboard.users[0].password)
+
+console.log(login_status)
 
 //Acción en consecuencia del login
-if (login == true){
+if (login_status === true){
     window.location.href = './menu.html';
 } else {
     alert("Usuario y/o contraseña son incorrectos. Inténtelo nuevamente.")
+    window.location.href = './?'
 }
