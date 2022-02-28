07181b6fe4609d8f2d083ae79beeda0c39ed30eb
Mariano Radusky <42505107+marianosin@users.noreply.github.com>
2022-02-28 19:44:05 -0300

    elimino console

--- JS/history.js
+++ JS/history.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
 
     dashboard.users[dashboard.activeUserPosition].opHistory.map((element)=>{
         $( `<tr class="inputHistory" ><td>${element.opId}</td><td>${element.ticker}</td><td>${element.netAmmount}</td><td>${element.price}</td><td>${element.quantity}</td><td>${element.commission}</td><td>${element.totalAmmount}</td><td>${element.exchange}</td><td>${element.eqType}</td><td>${element.currency}</td><td>${element.dateOperated}</td></tr>` ).appendTo( "#historyTableBody" );
-        console.log(element)
+        
     })
 });
 
